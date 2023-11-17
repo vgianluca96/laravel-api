9ecb7aa8028ef788d08fc2236ab9947d0ed9e0e0
vgianluca96 <gianluca.vallese@hotmail.com>
2023-11-17 19:25:20 +0100

    variato input paginate
    ridotto numero risultati per pagina per vedere pagine multiple

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,7 +10,7 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::orderByDesc('id')->paginate(5);
+        $projects = Project::orderByDesc('id')->paginate(3);
         return response()->json([
             'status' => 'success',
             'author' => 'gianluca',
